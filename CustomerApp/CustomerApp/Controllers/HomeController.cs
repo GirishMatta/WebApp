b08c525c311125e6c9f0fdd50af39c52f06e078c
Girish <girish.matta@hotmail.com>
2019-06-02 17:35:55 +1000

    Added HomeController with Index view

---
+++ CustomerApp/CustomerApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using CustomerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (DataContext db = new DataContext())
+            {
+                return View(db.Customers.ToList());
+            }
+        }
+    }
+}
