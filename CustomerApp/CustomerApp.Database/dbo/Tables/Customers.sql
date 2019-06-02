eb070867d46775c36e90f6e63171b44ef2affc5e
Girish <girish.matta@hotmail.com>
2019-06-02 18:24:10 +1000

    Added SSDT project

---
+++ CustomerApp/CustomerApp.Database/dbo/Tables/Customers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerID]   INT            IDENTITY (1, 1) NOT NULL,
+    [CustomerName] NVARCHAR (100) NOT NULL,
+    [City]         NVARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_dbo.Customers] PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
