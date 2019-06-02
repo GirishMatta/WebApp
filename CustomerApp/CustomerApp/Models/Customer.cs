313834ac08af3206f82d9c56700982e8367f7e5e
Girish <girish.matta@hotmail.com>
2019-06-02 17:17:14 +1000

    Added Customer class

---
+++ CustomerApp/CustomerApp/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [DisplayName("Customer Name")]
+        [StringLength(100)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string City { get; set; }
+    }
+}
