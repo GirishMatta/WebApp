0587eb03cff56c215839d3baa53abe4025c44021
Girish <girish.matta@hotmail.com>
2019-06-02 17:26:54 +1000

    Added a DataContext class with ConnectionString in the WebConfig

---
+++ CustomerApp/CustomerApp/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CustomerApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DataContext")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
